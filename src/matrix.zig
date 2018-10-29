2d9af8c34315d2b16d224bda248067ef6d5b026b
Wink Saville <wink@saville.com>
2018-10-28 18:12:17 -0700

    Add Matrix.mul and some refactoring
     - Rename Matrix.mat to Matrix.data.
     - Add Matrix.row_cnt and col_cnt.
     - Rename init to initVal and changed init to leave .data undefined

--- src/matrix.zig
+++ src/matrix.zig
@@ -7,22 +7,33 @@ pub fn Matrix(comptime T: type, comptime m: usize, comptime n: usize) type {
     return struct.{
         const Self = @This();
 
-        pub mat: [m][n]T,
+        pub row_cnt: usize,
+        pub col_cnt: usize,
+        pub data: [m][n]T,
 
         /// Initialize Matrix to a value
-        pub fn init(val: T) Self {
-            var r: Self = undefined;
+        pub fn init() Self {
+            return Self.{
+                .row_cnt = m,
+                .col_cnt = n,
+                .data = undefined,
+            };
+        }
+
+        /// Initialize Matrix to a value
+        pub fn initVal(val: T) Self {
+            var r = Self.init();
             return r.visit(fillFunc, val).*;
         }
 
         /// Initialize Matrix as a Unit matrix with 1's on the diagonal
         pub fn initUnit() Self {
             comptime if (m != n) @compileError("initUnit: not a square matrix");
-            var r: Self = undefined;
+            var r = Self.init();
             return r.visit(unitFunc, 0).*;
         }
 
-        /// Return true of pSelf.mat == pOther.mat
+        /// Return true of pSelf.data == pOther.data
         pub fn eql(pSelf: *Self, pOther: *const Self) bool {
             var eqlParam = EqlParam.{
                 .pOther = pOther,
@@ -30,6 +41,36 @@ pub fn Matrix(comptime T: type, comptime m: usize, comptime n: usize) type {
             };
             _ = pSelf.visit(eqlFunc, &eqlParam);
             return eqlParam.result;
+        }
+
+        /// Multiply two matrixes
+        pub fn mul(pSelf: *const Self, pOther: *const Self) !Self {
+            if (pSelf.row_cnt != pOther.col_cnt) {
+                return error.MulDoesNotResultInASqureMatrix;
+            }
+
+            var r = Matrix(T, m, m).init();
+            var i: usize = 0;
+            done: while (i < pSelf.row_cnt) : (i += 1) {
+                //warn("mul {}:\n", i);
+                var j: usize = 0;
+                while (j < pOther.col_cnt) : (j += 1) {
+                    //warn(" ({}:", j);
+                    comptime var k: usize = 0;
+                    inline while (k < m) : (k += 1) {
+                        var val = pSelf.data[i][k] * pOther.data[k][j];
+                        if (k == 0) {
+                            r.data[i][j] = val;
+                            //warn(" {}:{}={} * {}", k, val, pSelf.data[i][k], pOther.data[k][j]);
+                        } else {
+                            r.data[i][j] += pSelf.data[i][k] * pOther.data[k][j];
+                            //warn(" {}:{}={} * {}", k, val, pSelf.data[i][k], pOther.data[k][j]);
+                        }
+                    }
+                    //warn(" {})\n", r.data[i][j]);
+                }
+            }
+            return r;
         }
 
         /// Visit each matrix value starting at [0][0], [0][1] .. [m][n]
@@ -46,11 +87,11 @@ pub fn Matrix(comptime T: type, comptime m: usize, comptime n: usize) type {
             done: inline while (i < m) : (i += 1) {
                 //warn("visit {}:", i);
                 comptime var j: usize = 0;
-                inline while (j < m) : (j += 1) {
+                inline while (j < n) : (j += 1) {
                     if (!func(pSelf, i, j, param)) {
                         break :done;
                     }
-                    //warn(" {}", pSelf.mat[i][j]);
+                    //warn(" {}", pSelf.data[i][j]);
                 }
                 //warn("\n");
             }
@@ -60,7 +101,7 @@ pub fn Matrix(comptime T: type, comptime m: usize, comptime n: usize) type {
         /// Print the Matrix
         pub fn print(pSelf: *const Self, s: []const u8) void {
             warn("{}", s);
-            for (pSelf.mat) |row, i| {
+            for (pSelf.data) |row, i| {
                 warn("{}: []{}.{{ ", i, @typeName(T));
                 for (row) |val, j| {
                     warn("{.7}{} ", val, if (j < (row.len - 1)) "," else "");
@@ -70,12 +111,12 @@ pub fn Matrix(comptime T: type, comptime m: usize, comptime n: usize) type {
         }
 
         fn unitFunc(pSelf: *Self, i: usize, j: usize, param: var) bool {
-            pSelf.mat[i][j] = if (i == j) T(1) else T(0);
+            pSelf.data[i][j] = if (i == j) T(1) else T(0);
             return true;
         }
 
         fn fillFunc(pSelf: *Self, i: usize, j: usize, param: var) bool {
-            pSelf.mat[i][j] = param;
+            pSelf.data[i][j] = param;
             return true;
         }
 
@@ -85,20 +126,20 @@ pub fn Matrix(comptime T: type, comptime m: usize, comptime n: usize) type {
         };
 
         fn eqlFunc(pSelf: *Self, i: usize, j: usize, param: var) bool {
-            param.result = pSelf.mat[i][j] == param.pOther.mat[i][j];
-            //warn("eqlFunc: {}:{} l.mat:{} o.mat:{} result={}\n",
-            //    i, j, pSelf.mat[i][j], param.pOther.mat[i][j], param.result);
+            param.result = pSelf.data[i][j] == param.pOther.data[i][j];
+            //warn("eqlFunc: {}:{} l.data:{} o.data:{} result={}\n",
+            //    i, j, pSelf.data[i][j], param.pOther.data[i][j], param.result);
             return param.result; // Stop if not equal
         }
     };
 }
 
-test "mat.init" {
+test "matrix.init" {
     warn("\n");
-    var mf32 = Matrix(f32, 4, 4).init(1);
+    var mf32 = Matrix(f32, 4, 4).initVal(1);
     mf32.print("mf32: init(1)\n");
 
-    for (mf32.mat) |row| {
+    for (mf32.data) |row| {
         for (row) |val| {
             assert(val == 1);
         }
@@ -106,7 +147,7 @@ test "mat.init" {
 
     var mf64 = Matrix(f64, 4, 4).initUnit();
     mf64.print("mf64: initUnit\n");
-    for (mf64.mat) |row, i| {
+    for (mf64.data) |row, i| {
         for (row) |val, j| {
             if (i == j) {
                 assert(val == 1);
@@ -117,22 +158,54 @@ test "mat.init" {
     }
 }
 
-test "mat.eql" {
+test "matrix.eql" {
     warn("\n");
-    var m0 = Matrix(f32, 4, 4).init(0);
-    for (m0.mat) |row| {
+    var m0 = Matrix(f32, 4, 4).initVal(0);
+    for (m0.data) |row| {
         for (row) |val| {
             assert(val == 0);
         }
     }
-    var o0 = Matrix(f32, 4, 4).init(0);
+    var o0 = Matrix(f32, 4, 4).initVal(0);
     assert(m0.eql(&o0));
 
-    o0.mat[3][3] = 1;
-    o0.print("mat.eql: o0\n");
+    o0.data[3][3] = 1;
+    o0.print("data.eql: o0\n");
     assert(!m0.eql(&o0));
 
-    o0.mat[0][0] = 1;
-    o0.print("mat.eql: o0\n");
+    o0.data[0][0] = 1;
+    o0.print("data.eql: o0\n");
     assert(!m0.eql(&o0));
+}
+
+test "matrix.mul" {
+    warn("\n");
+    var m1 = Matrix(f32, 2, 2).init();
+    var m2 = Matrix(f32, 2, 2).init();
+    m1.data = [][2]f32.{
+        []f32.{ 1, 2 },
+        []f32.{ 3, 4 },
+    };
+    m2.data = [][2]f32.{
+        []f32.{ 5, 6 },
+        []f32.{ 7, 8 },
+    };
+    m1.print("matrix.mul m1:\n");
+    m2.print("matrix.mul m2:\n");
+    var m3 = try m1.mul(&m2);
+    m3.print("matrix.mul m3:\n");
+
+    var expected = Matrix(f32, 2, 2).init();
+    expected.data = [][2]f32.{
+        []f32.{
+            (m1.data[0][0] * m2.data[0][0]) + (m1.data[0][1] * m2.data[1][0]),
+            (m1.data[0][0] * m2.data[0][1]) + (m1.data[0][1] * m2.data[1][1]),
+        },
+        []f32.{
+            (m1.data[1][0] * m2.data[0][0]) + (m1.data[1][1] * m2.data[1][0]),
+            (m1.data[1][0] * m2.data[0][1]) + (m1.data[1][1] * m2.data[1][1]),
+        },
+    };
+    expected.print("matrix.mul expected:\n");
+    assert(m3.eql(&expected));
 }
